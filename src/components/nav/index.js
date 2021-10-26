bc3f932b82e0d77ec0f69cee4882869c0f5c1669
bimaaghafara <bimaa7@gmail.com>
2021-10-26 11:59:48 +0700

    onprogress nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,26 +1,42 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Nav = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Home
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton className="testss" edge="start" color="inherit" aria-label="menu"
+                        sx={{
+                            mr: 2,
+                            display: {
+                                md: 'none'
+                            }
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box
+                        sx={{
+                            display: {
+                                xs: 'none',
+                                md: 'block'
+                            }
+                        }}
+                    >
+                        <Button color="inherit">Home</Button>
+                        <Button color="inherit">Products</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
 export default Nav;
