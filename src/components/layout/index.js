9580d14666ba94740200afb860b0baa0abffc0e2
bimaaghafara <bimaa7@gmail.com>
2021-11-05 00:36:21 +0700

    floating button

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,6 +2,8 @@ import Nav from 'src/components/nav';
 import Footer from 'src/components/footer';
 import Container from '@mui/material/Container';
 import PropTypes from 'prop-types';
+import Fab from '@mui/material/Fab';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 const Layout = ({
     children, maxWidth
 }) => {
@@ -11,6 +13,15 @@ const Layout = ({
             <Container maxWidth={maxWidth} sx={{ mt: {xs: '56px', md: '64px'} }}>
                 {children}
             </Container>
+            <Fab size="large" aria-label="WA" sx={{
+                position: 'fixed',
+                right: ['20px', '40px'],
+                bottom: ['20px', '30px'],
+                color: '#fff',
+                background: '#25d366'
+            }}>
+                <WhatsAppIcon />
+            </Fab>
             <Footer />
         </>
     )
