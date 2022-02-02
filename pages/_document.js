d1ad378d864905fa167f2754f711f26a5538afb5
bimaaghafara <bimaa7@gmail.com>
2022-02-02 23:45:02 +0700

    add google-site-verification

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+            <meta name="google-site-verification" content="qiS-YxKx6eLgAcrWrS_Z3B5UahfgPjl9vdVLwnRR3SY" />
             <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         </Head>
